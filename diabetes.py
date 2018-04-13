026b67783e4230934e11548dd564b07a6803424a
Abhishek Ranjan 3 <abhranja1@publicisgroupe.net>
2018-04-13 22:43:29 +0530

    Comments added to the files

--- diabetes.py
+++ diabetes.py
@@ -1,21 +1,29 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-df1=pd.read_csv("D:\diabetes.csv")
-df1
-df1.head()
-df1.tail()
-df1.describe()
-x=df['Glucose']
-y=df['BloodPressure']
+# Reading CSV from file system.
+df=pd.read_csv("D:\diabetes.csv")
+df
+#Getting Top 5 element of the Excel data.
+df.head()
+#Getting Bottom 5 element of the Excel data.
+df.tail()
+#Getting Mean Median and mode and description.
+df.describe()
+#Created 3 Variables x,y,z
+x=df['Pregnancies']
+y=df['SkinThickness']
 z=df['Outcome']
-o=df['']
+#Histogram for x,y
 plt.hist(x)
 plt.hist(y)
 plt.show()
+#2 variable Scattered diagram for X,Y
 plt.scatter(x,y)
 plt.show()
+#Co relation Matrix 
 df.corr()
+
 colors={'yes':'red','no':'green'}
 colors=['yes']
 plt.scatter(x,y, c=z.apply(lambda x:colors[x]))
